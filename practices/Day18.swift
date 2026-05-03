995e04dc4a57baf6534037e3b177963371f12033
Ralph <gonzagaralphdainiell@gmail.com>
2026-05-03 21:23:14 +0800

    Practices

--- practices/Day18.swift
+++ practices/Day18.swift
@@ -1,14 +1,34 @@
-let userName: String = "Ralph"
-var profileBio: String? = nil 
+// let userName: String = "Ralph"
+// var profileBio: String? = nil 
 
-profileBio = "Aspiring iOS Developer"
+// profileBio = "Aspiring iOS Developer"
+
+// print("User: \(userName)")
+
+// if let bio = profileBio {
+//     print("Bio: \(bio)")
+    
+// }else {
+//     print("Bio: ")
+    
+// }
+
+
+
+// task 2
+
+let userName: String = "Ralph"
+let appName: String = "FitTrack"
+var recoveryEmail: String? = nil
+
 
 print("User: \(userName)")
+print("App: \(appName)")
 
-if let bio = profileBio {
-    print("Bio: \(bio)")
+if let email = recoveryEmail {
+    print("Recovery Email: \(email)")
     
 }else {
-    print("Bio: ")
+    print("Recovery Email: No recovery email set")
     
 }
