4453f925299fdb711972d381dc0952705956da8c
Ralph <gonzagaralphdainiell@gmail.com>
2026-05-05 23:15:38 +0800

    practices

--- practices/Day21.swift
+++ practices/Day21.swift
@@ -18,3 +18,28 @@ if let quantity = Int(quantityText) {
     print("Invalid Computation")
     
 }
+
+
+
+//Task 2
+let itemName: String = "Keyboard Cover"
+let price: Double = 300.0
+let quantityText: String = "3"
+let discount: Double = 50.0
+
+
+print("Item: \(itemName)")
+
+
+if let quantity = Int(quantityText) {
+    print("Quantity: \(quantity)")
+    
+    let subTotal: Double = price * Double(quantity)
+        print("Subtotal: \(subTotal)")
+    
+    let finalTotal: Double = subTotal - discount
+        print("Final Total: \(finalTotal)")
+        
+}else {
+    print("Invalid Input ")
+}
