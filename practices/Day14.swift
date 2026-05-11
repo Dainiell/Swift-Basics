d25c53572406bda96b1f6f3fba5fb78006c9deb5
Ralph <gonzagaralphdainiell@gmail.com>
2026-05-11 22:44:29 +0800

    Arrays

--- practices/Day14.swift
+++ practices/Day14.swift
@@ -1,16 +1,43 @@
-let userName: String = "Ralph"
-var membershipLevel: String? = nil
+var downloads: [String] = ["ChatApp", "WeatherPro", "ChatApp", "NotesPlus", "ChatApp", "WeatherPro", "BudgetMate"]
 
-membershipLevel = "Gold"
+var uniqueApps: [String] = []
 
-print("User: \(userName)")
-
-
-if let subscription = membershipLevel {
-    print("Membership: \(subscription)")
-    
-}else {
-    print("Membership: None")
-    
+for app in downloads {
+    if !uniqueApps.contains(app) {
+        uniqueApps.append(app)
+    }
 }
-//end 
+
+for app in uniqueApps {
+    var count = 0
+    
+    for download in downloads {
+        if download == app {
+            count += 1
+        }
+    }
+    
+    print("\(app): \(count)")
+}
+
+var mostDL = ""
+var highestCount = 0
+
+for app in uniqueApps {
+    var count = 0
+    
+    for download in downloads {
+        if download == app {
+            count += 1
+        }
+    }
+    
+    if count > highestCount {
+        highestCount = count
+        mostDL = app
+    }
+}
+
+print("Most Downloaded: \(mostDL) (\(highestCount) downloads)")
+print("Cleaned Apps: \(uniqueApps)")
+print("Unique Apps: \(uniqueApps.count)")
