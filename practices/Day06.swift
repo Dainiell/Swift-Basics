659ae0fc3c66d3920beaab7d96d3b7a61c172c0e
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-21 13:50:14 +0800

    Function in Swift

--- practices/Day06.swift
+++ practices/Day06.swift
@@ -1,41 +1,14 @@
-for rd in 1...5 
-{
-    print("Hello \(rd)")
-    
+var goldBars = 0
+
+func unlockTreasureChest (inventory: Int) -> Int {
+    inventory + 100
+
 }
-// ----------------
-let numberPlate = "WW87GP"
-for character in numberPlate {
-  print("character is = \(character)")
-}
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
 
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
 
-// ----------------
-var turns = 1 
-
-while turns <= 3 
-{
-    print("This is turn number \(turns)")
-    turns += 1
-}
-
-// ----------------
-var totalturns = 4
-var leftturns = true 
-
-while leftturns 
-{
-    totalturns -= 1 
-    if totalturns < 1 {
-        leftturns = false 
-    }
-    print("got another turn")
-      
-    
-}
-// ----------------
-for dice in 1...6 {
-    print("Roll a \(dice)")
-    
-}  
-
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
