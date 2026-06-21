8faecbfa415f453ac2e778c61a3fe15b84f80047
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-21 09:07:07 +0800

    Dictionaries in Swift

--- practices/Day05.swift
+++ practices/Day05.swift
@@ -1,51 +1,29 @@
-let freeApp = true
+let levelScores = ["1": 10, "2": 20, "3": 30, "4": 40, "5": 50, "6": 60, "7": 70] 
 
-if freeApp == true {
-    print("You are using the free version of the app. Buy the full version of the app to get access to all of its features.")
+for (level,score) in levelScores {
+    print("The score of level \(level) is \(score).")
     
 }
+var gameScore = 0
 
-
-
-
-
-let morningTemperature = 70
-let eveningTemperature = 80
-
-if morningTemperature < eveningTemperature {
-  print("It is colder in the morning.")
-} else {
-  print("It is colder in the evening.")
+for (_, score) in levelScores {
+    gameScore += score
 }
 
+print("Final game score: \(gameScore)")
+
+let weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95, "Sunday": 100] 
 
 
-
-
-let temperatureDegree = "Fahrenheit" 
+for (day,temp) in weeklyTemperatures {
+    print("Today is \(day) and its currently \(temp) ")
+    
+}
  
-if temperatureDegree == "Fahrenheit" {
-    print("App uses Fahrenheit degrees.")
-    
-}else {
-    print("App uses Celsius degrees.")
-    
-}
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] 
 
-if temperatureDegree == "Fahrenheit" || temperatureDegree == "Celsius" {
-    print("App is configured properly.")
-    
-}else {
-    print("App isn’t configured properly.")
-    
-}
+let temperatures = [70, 75, 80, 85, 90, 95, 100] 
 
-
-
-
-switch temperatureDegree {
-    case "Fahrenheit": print("The app is configured for the US")
-    case "Celsius": print("The app is configured for Europe")
-    default: print("The app has an unknown configuration in this case.")
-
+for index in 0...6 {
+    print("\(days[index]) temperature is \(temperatures[index]).")
 }
