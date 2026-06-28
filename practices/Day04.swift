6758891d54085a39be98abfbd0f34e0570020fa2
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-28 18:38:45 +0800

    Keep going

--- practices/Day04.swift
+++ practices/Day04.swift
@@ -14,3 +14,70 @@ for level in 1...levels {
     }
     
 }
+
+
+let quizScore = 68 
+
+if quizScore <= 100 && quizScore >= 90 {
+    print("Excellent")
+    
+}else if quizScore <= 89 && quizScore >= 75 {
+    print("Good Job")
+    
+}else if quizScore <= 74 && quizScore >= 60 {
+    print("Needs Review")
+    
+}else  {
+    print("Study Again")
+    
+}
+
+switch quizScore {
+    case 90...100: print("Excellent")
+    case 75...89: print("Good Job")
+    case 60...74: print("Needs Review")
+    default: print("Study Again")
+    
+}
+
+
+
+let ingredient = 10
+
+if ingredient == 0 {
+    print("Out of Stock")
+    
+}else if ingredient <= 3 {
+    print("Low Stock")
+    
+}else {
+    print("In Stock")
+    
+} 
+
+
+
+let category = "Dessert"
+
+switch category {
+    case "breakfast": print("🥪")
+    case "Lunch": print("🍽️")
+    case "Dessert": print("🥯")
+    case "Snack": print("🥐")
+    default: print("Please select your category")   
+}
+
+
+
+
+
+let streakDays = 1
+
+switch streakDays {
+ case 1...2: print("Beginner")
+ case 3...6: print("Consistent")
+ case 7...13: print("Dedicated")
+ case 14...20: print("Unstoppable")
+ default: print("Do more keep it up!")
+    
+}
