329fecbe0844675409aaf2a17f267149ad3212bc
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-29 22:41:51 +0800

    Keep Going

--- practices/Day07.swift
+++ practices/Day07.swift
@@ -1,22 +1,139 @@
-var goldBars = 0
+var guess = 1
 
-
-func incrementInventory(_ inventory: inout Int, by amount: Int = 100) {
-  inventory += amount
+repeat {
+  print(guess) 
+  guess += 2 
+  
 }
+while guess <= 7
 
 
-incrementInventory(&goldBars)
-print(goldBars)
 
-incrementInventory(&goldBars)
-print(goldBars)
 
-incrementInventory(&goldBars)
-print(goldBars)
 
-incrementInventory(&goldBars, by: 300)
-print(goldBars)
+var sum = 0 
+var number = 1
 
-incrementInventory(&goldBars, by: 50)
-print(goldBars)
+repeat {
+    sum += number
+    print("\(sum)")
+    number += 2
+
+}while number <= 9 
+
+
+
+
+
+var multiplier = 1 
+
+
+repeat {
+    let result = 3 * multiplier 
+    print("3 x \(multiplier) = \(result)")
+    multiplier += 1
+
+
+}while multiplier <= 5
+
+
+
+
+
+
+var count = 10 
+
+repeat {
+  print("\(count)")
+  count -= 1
+
+
+}while count >= 1
+
+print("Liftoff!")
+
+
+
+
+
+
+var num = 2
+ 
+
+ repeat {
+    print("\(num)")
+    num += 2
+    
+
+
+ }while num <= 20
+
+
+
+
+
+
+
+
+
+
+var value = 100
+var divisions = 0
+
+
+repeat {
+    value = value / 2
+    divisions += 1
+
+
+}while value > 1
+
+
+print("\(divisions)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var n = 1
+
+repeat {
+  print("\(n) squared is \(n*n)")
+  n += 1
+
+
+}while n <= 5
+
+
+
+
+
+
+
+
+
+
+var word = "Swift"
+var index = 0 
+var charCount = 0
+
+
+repeat {
+  charCount += 1
+  index += 1
+
+}while index < word.count
+
+
+print("\(charCount)")
