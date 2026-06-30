fad57ac86b0996530980eb12a6eaac776c58d9bb
Ralph <gonzagaralphdainiell@gmail.com>
2026-06-30 22:52:16 +0800

    Keep going

--- practices/Day09.swift
+++ practices/Day09.swift
@@ -1,13 +1,43 @@
-var goldBars = 100
+var username: String = "Ralph"
+var password: String = "ralph-dev31"
+var storedEmail: String? = nil
 
-func spendTenGoldBars(from inventory: inout Int, completion: (Int) -> Void) {
-    inventory -= 10
-    completion(inventory)
+
+if password.count >= 8 {
+    print("Valid password")
+    
+}else {
+    print("Password too short")
+    
 }
 
-print("You had \(goldBars) gold bars.")
+if let email = storedEmail {
+    print("Welcome back \(email)")
+   
+    
+    }else { 
+    print("Please enter your email")
+    }
 
-spendTenGoldBars(from: &goldBars) { goldBars in
-    print("You spent ten gold bars.")
-    print("You have \(goldBars) gold bars.")
+var loginAttempts = 0
+
+while loginAttempts < 3 {
+    loginAttempts += 1
+    print("Attempt: \(loginAttempts) of 3")
+    
+}
+
+
+var failedLogins: [String] = ["rralph"]
+
+for login in failedLogins {
+    print("Failed Login: \(login)")
+}
+
+
+switch loginAttempts {
+    case 0: print("No attempts yet")
+    case 1...2: print("Try again")
+    default: print("Account locked")
+    
 }
